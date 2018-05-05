462ee22153b0cae318583f6cdb24e2c17c1db178
Martin Jaggi <m.jaggi@gmail.com>
2018-05-05 22:24:20 +0200

    publish python template lab 09

---
+++ labs/ex09/template/helpers.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""some helper functions for project 1."""
+import csv
+import numpy as np
+
+
+def load_csv_data(data_path, sub_sample=False):
+    """Loads data.
+    return
+        y(class labels), tX (features) and ids (event ids).
+    """
+    y = np.genfromtxt(
+        data_path, delimiter=",", skip_header=1, dtype=str, usecols=1)
+    x = np.genfromtxt(
+        data_path, delimiter=",", skip_header=1)
+    ids = x[:, 0].astype(np.int)
+    input_data = x[:, 2:]
+
+    # convert class labels from strings to binary (-1,1)
+    yb = np.ones(len(y))
+    yb[np.where(y == 'b')] = -1
+
+    # sub-sample
+    if sub_sample:
+        yb = yb[::50]
+        input_data = input_data[::50]
+        ids = ids[::50]
+
+    return yb, input_data, ids
+
+
+def predict_labels(weights, data):
+    """Generates class predictions given weights, and a test data matrix."""
+    y_pred = np.dot(data, weights)
+    y_pred[np.where(y_pred <= 0)] = -1
+    y_pred[np.where(y_pred > 0)] = 1
+
+    return y_pred
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
